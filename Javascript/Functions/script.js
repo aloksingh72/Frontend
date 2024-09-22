cdf541d06b10fa527dbaa12cd860ecb38e9e4570
Alok Singh <aloksingh36757@gmail.com>
2024-09-22 14:33:49 +0530

    javascript updated

---
+++ Javascript/Functions/script.js
@@ -0,0 +1,48 @@
+/*function printName(){
+    console.log("hello world!")
+
+}
+printName();
+*/
+
+/*function with arguments  
+function fullName(firstName,lastName){
+    let fullName = firstName + " " + lastName;
+    return fullName;
+
+}
+let Name =fullName("alok","singh");
+console.log(Name);
+*/
+
+
+/*
+call back function 
+
+function company (call,name,city){
+    call(name,city);
+    console.log("hello world!");
+}
+
+function user(name,city){
+    console.log(`user name ${name}`);
+    console.log(`city ${city}`);
+}
+
+console.log(company(user,"Tinku","noida"));
+*/
+
+
+
+const genric=(printName,userName)=>{
+    printName(userName);
+};
+
+
+const printName=(userName)=>{
+    console.log(`user name is ${userName}`);
+    return userName ;
+}
+
+let ans = genric(printName,"Nobita");
+console.log(ans);
