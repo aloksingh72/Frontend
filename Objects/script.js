102b3bdc67a7ad3fc2d0b5248f22df65ca131ff2
Alok Singh <aloksingh36757@gmail.com>
2024-09-22 19:52:10 +0530

    array in javascript

---
+++ Objects/script.js
@@ -0,0 +1,59 @@
+
+/*
+// Creating an object
+const obj={
+    name:"Alok Singh",
+    age:20,
+    city:"Noida"
+}
+//  accesing the object key value pairs 
+console.log(obj.name);
+// another ways 
+console.log(obj["city"]);
+*/
+
+// -----> shallow copy
+const jack={
+    cake:"Chocolate cake",
+
+}
+const oggy = jack;
+console.log(oggy.cake);
+console.log(oggy["cake"])
+
+/*
+//Creation of Array
+
+// By using array literals
+let arr = [1,5,5,9,5,56,8];
+console.log(arr);
+
+// By using array Constructors
+
+let arr2 = new Array('alok',720,true);
+console.log(arr2);
+
+let type=typeof(arr2);
+console.log(type);
+
+// Pushing Values into arrays
+arr2.push("singh");
+console.log(arr2);
+arr2.pop();
+console.log(arr2);
+
+arr2.push(30);
+arr2.push(45);
+arr2.push(85);
+console.log(arr2);
+arr2.splice(1,2,"tinku");
+console.log(arr2);
+
+
+let arr3 = new Array('alok','singh','Noida',720,true);
+console.log(arr3);
+let ans = arr3.slice(1,3);
+console.log(ans);
+
+*/
+
